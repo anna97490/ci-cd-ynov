e1ee4c68c5ebc786c7e588296b420fcbad2a4f46
fleurkernevez <fleur@transparence.pro>
2025-03-21 09:52:48 +0100

    cours du 27/2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { fireEvent } from '@testing-library/react';
 
-test('renders learn react link', () => {
+test.skip('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('check counter on click me button', () => {
+  render(<App />);
+  const button = screen.getByRole('button');
+  const counter = screen.getByTestId('count')
+  expect(button).toBeInTheDocument();
+  expect(counter).toBeInTheDocument();
+  expect(counter).toHaveTextContent("0");
+  fireEvent.click(button);
+  expect(counter).toHaveTextContent("1");
+ });
